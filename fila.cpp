acb209f78dc1537ca8a644b8ae896ab38b55b8f1
Leonardo Gideão Costa Rocha <gideonveloz@gmail.com>
2020-09-06 14:01:25 -0300

    Update fila.cpp

--- fila.cpp
+++ fila.cpp
@@ -1,4 +1,4 @@
-// Copyright [2018] <COLOQUE SEU NOME AQUI...>
+// Copyright [2020] Leonardo Gideão Costa Rocha
 #ifndef STRUCTURES_ARRAY_QUEUE_H
 #define STRUCTURES_ARRAY_QUEUE_H
 
@@ -46,71 +46,69 @@ class ArrayQueue {
 }  // namespace structures
 
 #endif
-
+// construtor vazio
 template<typename T>
 structures::ArrayQueue<T>::ArrayQueue() {
     ArrayQueue(DEFAULT_SIZE);
 }
-
+// construtor com parametro de tamanho
 template<typename T>
 structures::ArrayQueue<T>::ArrayQueue(std::size_t max) {
     max_size_ = max;
-    contents = new T[max_size_];
     begin_ = -1;
     end_ = -1;
     size_ = 0;
+    contents = new T[max_size_];
 }
-
+// destrutor
 template<typename T>
 structures::ArrayQueue<T>::~ArrayQueue() {
-  delete [] contents;
+    delete [] contents;
 }
-
+// metodo limpar fila
 template<typename T>
 void structures::ArrayQueue<T>::clear() {
-  begin_ = -1;  // marcando o inicio e fim da fila para posicao inicial
-  end_ = -1;
-  size_ = 0;
+    end_ = -1;
+    begin_ = -1;
+    size_ = 0;
 }
-
+// metodo verificar se esta vazia
 template<typename T>
 bool structures::ArrayQueue<T>::empty() {
     return size_ == 0;
 }
-
+// metodo verificar se esta cheia
 template<typename T>
 bool structures::ArrayQueue<T>::full() {
     return size_ == max_size_;
 }
-
+// retornar tamanho atual
 template<typename T>
 std::size_t structures::ArrayQueue<T>::size() {
-    return size_;  // tamanho atual da pilha
+    return size_;
 }
-
+// retornar tamanho maximo da fila
 template<typename T>
 std::size_t structures::ArrayQueue<T>::max_size() {
-    return max_size_;  // tamanho maximo que a pilha pode alcançar
+    return max_size_;
 }
-
+// retonar ultimo elemento da lista
 template<typename T>
 T& structures::ArrayQueue<T>::back() {
-    // mostrando o ultimo da fila
     if (empty()) throw std::out_of_range("fila vazia");
     return contents[end_];
 }
-
+// adicionando elemento na lista
 template<typename T>
 void structures::ArrayQueue<T>::enqueue(const T& data) {
     if (full()) throw std::out_of_range("fila cheia");
     size_++;
-    contents[++end_] = data;
+    contents[++end_%max_size()] = data;
 }
-
+// removendo elemento da fila
 template<typename T>
 T structures::ArrayQueue<T>::dequeue() {
     if (empty()) throw std::out_of_range("fila vazia");
     size_--;
-    return contents[++begin_];
+    return contents[++begin_%max_size()];
 }
-
